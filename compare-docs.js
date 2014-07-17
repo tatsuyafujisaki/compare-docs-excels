2ff55f35b65d180ded37a4c0f6cb375bb52f8705
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2014-07-17 22:06:25 +0900

    First commit

---
+++ compare-docs.js
@@ -0,0 +1,20 @@
+var wdPaneRevisions = 18
+
+if (WScript.Arguments.Length != 2) {
+    WScript.Echo("Please drag & drop two files.")
+}
+
+var app = WScript.CreateObject("Word.Application")
+var doc1 = app.Documents.Open(WScript.Arguments(0))
+var doc2 = app.Documents.Open(WScript.Arguments(1))
+
+app.Application.CompareDocuments(doc1, doc2)
+
+doc1.Close()
+doc2.Close()
+
+do {
+    app.ActiveWindow.View.SplitSpecial = wdPaneRevisions
+} while (app.ActiveWindow.View.SplitSpecial != wdPaneRevisions)
+
+app.Visible = true
