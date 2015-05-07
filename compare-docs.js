56f86835ac54554e05fc3bd30007c230d722a4eb
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2015-05-07 22:56:25 +0900

    Add semicolons.

--- compare-docs.js
+++ compare-docs.js
@@ -1,21 +1,20 @@
 if (WScript.Arguments.Length != 2) {
-    WScript.Echo("Drag & drop two Microsoft Word files.")
+    WScript.Echo("Please drag & drop two Microsoft Word files.");
 }
 
-var app = WScript.CreateObject("Word.Application")
-var doc1 = app.Documents.Open(WScript.Arguments(0))
-var doc2 = app.Documents.Open(WScript.Arguments(1))
+var app = WScript.CreateObject("Word.Application");
+var doc1 = app.Documents.Open(WScript.Arguments(0));
+var doc2 = app.Documents.Open(WScript.Arguments(1));
 
-app.Application.CompareDocuments(doc1, doc2)
+app.Application.CompareDocuments(doc1, doc2);
 
-doc1.Close()
-doc2.Close()
+doc1.Close();
+doc2.Close();
 
-var wdPaneRevisions = 18
+var wdPaneRevisions = 18;
 
-// Once is not enough
 do {
-    app.ActiveWindow.View.SplitSpecial = wdPaneRevisions
+    app.ActiveWindow.View.SplitSpecial = wdPaneRevisions;
 } while (app.ActiveWindow.View.SplitSpecial != wdPaneRevisions)
 
-app.Visible = true
+app.Visible = true;
