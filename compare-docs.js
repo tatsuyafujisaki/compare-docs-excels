9f8213ae3e488cb695ef71cc41000183e32e6b58
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2016-06-21 22:47:15 +0900

    Simplify compare-docs.js

--- compare-docs.js
+++ compare-docs.js
@@ -7,15 +7,12 @@ var app = WScript.CreateObject("Word.Application");
 var doc1 = app.Documents.Open(WScript.Arguments.Item(0));
 var doc2 = app.Documents.Open(WScript.Arguments.Item(1));
 
-app.Application.CompareDocuments(doc1, doc2);
+app.CompareDocuments(doc1, doc2);
 
 doc1.Close();
 doc2.Close();
 
 var wdPaneRevisions = 18;
 
-do {
-  app.ActiveWindow.View.SplitSpecial = wdPaneRevisions;
-} while (app.ActiveWindow.View.SplitSpecial !== wdPaneRevisions)
-
+app.ActiveWindow.View.SplitSpecial = wdPaneRevisions;
 app.Visible = true;
