abb166bae798cb6c8788535b421b370edfc44552
Tatsuya Fujisaki <tatsuya.fujisaki@gmail.com>
2018-08-09 23:11:34 +0900

    Update compare-excels.js

--- compare-excels/compare-excels.js
+++ compare-excels/compare-excels.js
@@ -1,5 +1,9 @@
 "use strict";
 
+function doubleQuote(s) {
+  return '"' + s + '"';
+}
+
 if (WScript.Arguments.length !== 2) {
   WScript.Echo('Please drop two Excel files.');
   WScript.Quit();
@@ -17,8 +21,11 @@ file.WriteLine(WScript.Arguments(0));
 file.WriteLine(WScript.Arguments(1));
 file.Close();
 
-// If AppVLP.exe (Microsoft Application Virtualization) is used.
-shell.Run('"C:\\Program Files\\Microsoft Office\\root\\Client\\AppVLP.exe" "C:\\Program Files (x86)\\Microsoft Office\\Office16\\DCF\\SPREADSHEETCOMPARE.EXE"' + temporaryFile);
+var appvlp = 'C:\\Program Files\\Microsoft Office\\root\\Client\\AppVLP.exe'
+var spreadsheetcompare = 'C:\\Program Files (x86)\\Microsoft Office\\Office16\\DCF\\SPREADSHEETCOMPARE.EXE'
 
-// If not AppVLP.exe (Microsoft Application Virtualization) is used.
-shell.Run('"C:\\Program Files (x86)\\Microsoft Office\\Office16\\DCF\\SPREADSHEETCOMPARE.EXE"' + temporaryFile);
+if (fso.FileExists(appvlp)) {
+  shell.Run(doubleQuote(appvlp) + ' ' + doubleQuote(spreadsheetcompare) + ' ' + temporaryFile);
+} else {
+  shell.Run(doubleQuote(spreadsheetcompare) + ' ' + temporaryFile);
+}
